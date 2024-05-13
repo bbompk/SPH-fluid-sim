290442e28f20f73de6567aa63c00122751c8f019
panithi.k <panithi.khamwangyang@agoda.com>
2024-05-13 18:11:13 +0700

    feat: more gui

--- src/js/script.js
+++ src/js/script.js
@@ -10,55 +10,71 @@ document.body.appendChild( renderer.domElement );
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const orbit = new OrbitControls( camera, renderer.domElement );
 
-const axesHelper = new THREE.AxesHelper( 5 );
-scene.add( axesHelper );
-
-camera.position.set( 0, 0, 12 )
-orbit.update();
+camera.position.set( 0, 0, 11 )
 
 const datgui = new dat.GUI();
 
-const CANVAS_WIDTH = 24;
-const CANVAS_HEIGHT = 16;
-
-const BOUND_X = 24
-const BOUND_Y = 16
+const MAX_WIDTH = 24;
+const MAX_HEIGHT = 16;
 
 const PARTICLE_DIV = 10;
 const PARTICLE_RADIUS = 0.1;
 const NUM_PARTICLES = 1600;
-const PARTICLE_INIT_DIMENSION = new THREE.Vector2(40, 40)
-if (PARTICLE_INIT_DIMENSION.x * PARTICLE_INIT_DIMENSION.y != NUM_PARTICLES) throw new Error('PARTICLE_INIT_DIMENSION must be a factor of NUM_PARTICLES')
+const PARTICLE_INIT_WIDTH = 40;
 if (PARTICLE_DIV < 3) throw new Error('PARTICLE_DIV must be greater than 2');
 
 // serializable options
 const options = {
-    gravity: 0,
+    gravity: 5,
     collisionDamping: 0.85,
-    smoothingRadius: 0.85,
+    smoothingRadius: 0.35,
     particleMass: 1,
     targetDensity: 1.2,
-    pressureMultiplier: 1,
-    useSpatialGridLookup: true
+    pressureMultiplier: 32,
+    useSpatialGridLookup: true,
+    boundV: MAX_HEIGHT,
+    boundH: MAX_WIDTH
 }
 
 // variables
-var spatialGridRows = Math.ceil(BOUND_Y / options.smoothingRadius);
-var spatialGridCols = Math.ceil(BOUND_X / options.smoothingRadius);
+var spatialGridRows = Math.ceil(options.boundV/ options.smoothingRadius);
+var spatialGridCols = Math.ceil(options.boundH / options.smoothingRadius);
+var paused = true;
 
 datgui.add(options, 'gravity', 0, 20)
 datgui.add(options, 'collisionDamping', 0, 1)
 datgui.add(options, 'smoothingRadius', 0.05, 5).onChange(() => {
-    spatialGridRows = Math.ceil(BOUND_Y / options.smoothingRadius);
-    spatialGridCols = Math.ceil(BOUND_X / options.smoothingRadius);
+    spatialGridRows = Math.ceil(options.boundV / options.smoothingRadius);
+    spatialGridCols = Math.ceil(opitons.boundH / options.smoothingRadius);
     console.log(spatialGridRows, spatialGridCols);
 })
 datgui.add(options, 'particleMass', 0.1, 10)
 datgui.add(options, 'targetDensity', 0.1, 30)
-datgui.add(options, 'pressureMultiplier', 0.1, 40)
+datgui.add(options, 'pressureMultiplier', 0.1, 200)
 datgui.add(options, 'useSpatialGridLookup')
+datgui.add(options, 'boundV', 2, MAX_HEIGHT, 1)
+datgui.add(options, 'boundH', 2, MAX_WIDTH, 1)
+
+function pause(forcePause = false) {
+    paused = !paused;
+    if (forcePause) paused = true;
+    if (paused) {
+        toggleBtn.innerText = 'PLAY';
+        toggleBtn.classList.add('paused');
+        toggleBtn.classList.remove('unpaused')
+    } else {
+        toggleBtn.innerText = 'PAUSE';
+        toggleBtn.classList.remove('paused');
+        toggleBtn.classList.add('unpaused');
+    }
+}
+
+const toggleBtn = document.getElementById('toggle-btn')
+toggleBtn.onclick = () => {
+    pause();
+}
+
 
 class Particle {
     constructor(pos) {
@@ -74,7 +90,7 @@ class Particle {
         particleVertices = new Float32Array( particleVertices );
         particleGeometry.setIndex( particleIndices );
         particleGeometry.setAttribute( 'position', new THREE.BufferAttribute( particleVertices, 3 ) );
-        const particleMat = new THREE.MeshBasicMaterial( { color: 0x0033ff, side: THREE.DoubleSide } );
+        const particleMat = new THREE.MeshBasicMaterial( { color: 0x0033ff } );
         const particle = new THREE.Mesh( particleGeometry, particleMat );
         particle.position.set( pos.x, pos.y, pos.z );
 
@@ -86,13 +102,13 @@ class Particle {
     }
 }
 
-const planeGeometry = new THREE.PlaneGeometry( CANVAS_WIDTH, CANVAS_HEIGHT );
+const planeGeometry = new THREE.PlaneGeometry( MAX_WIDTH, MAX_HEIGHT );
 const planeMat = new THREE.MeshBasicMaterial( { color: 0xEFEFEF, side: THREE.DoubleSide } );
 const plane = new THREE.Mesh( planeGeometry, planeMat );
-scene.add( plane );
+//scene.add( plane );
 plane.position.set( 0, 0, -1 );
 
-const particles = [];
+var particles = [];
 const densities = new Array(NUM_PARTICLES).fill(0);
 const positions = new Array(NUM_PARTICLES).fill(new THREE.Vector3(0));
 const velocities = new Array(NUM_PARTICLES).fill(new THREE.Vector3(0));
@@ -101,20 +117,38 @@ var spatialLookupTable = (new Array(NUM_PARTICLES).fill(0)).map(() => new THREE.
 var spatialTableStartIndices = new Array(NUM_PARTICLES).fill(-1);
 
 // Initialize particles
-const halfx = Math.ceil(PARTICLE_INIT_DIMENSION.x / 2);
-const halfy = Math.ceil(PARTICLE_INIT_DIMENSION.y / 2);
-var pidx = 0;
-for (let i = -halfx; i <= halfx; i++) {
-    for (let j = -halfy; j <= halfy; j++) {
-        if (pidx >= NUM_PARTICLES) break;
-        const particle = new Particle( new THREE.Vector3( i * PARTICLE_RADIUS * 2, j * PARTICLE_RADIUS * 2, 0 ) );
-        scene.add( particle.sceneObject );
-        particles.push( particle );
-        positions[pidx] = particle.position.clone();
-        velocities[pidx] = particle.velocity.clone();
-        pidx++;
+function reset() {
+    particles.forEach(particle => {
+        scene.remove(particle.sceneObject);
+    })
+    particles = [];
+
+    const halfx = Math.ceil(PARTICLE_INIT_WIDTH / 2);
+    const halfy = Math.ceil((NUM_PARTICLES / PARTICLE_INIT_WIDTH) / 2);
+    var pidx = 0;
+    for (let i = -halfx; i <= halfx; i++) {
+        for (let j = -halfy; j <= halfy; j++) {
+            if (pidx >= NUM_PARTICLES) break;
+            const particle = new Particle( new THREE.Vector3( i * PARTICLE_RADIUS * 2, j * PARTICLE_RADIUS * 2, 0 ) );
+            scene.add( particle.sceneObject );
+            particles.push( particle );
+            positions[pidx] = particle.position.clone();
+            velocities[pidx] = particle.velocity.clone();
+            pidx++;
+        }
     }
+
+    console.log(`Initialized ${particles.length} particles`)
+
+    renderer.render( scene, camera );
 }
+
+const resetBtn = document.getElementById('reset-btn');
+resetBtn.onclick = () => {
+    reset();
+    pause(true);
+}
+
 
 
 function getCellCoord(pos) {
@@ -133,7 +167,7 @@ function updateSpatialLookupTable() {
 
     spatialTableStartIndices = new Array(spatialGridSize).fill(-1);
     for (let i = 0; i < NUM_PARTICLES; i++) {
-        const pos = positions[i].clone();
+        const pos = predictedPositions[i].clone();
         const cellCoord = getCellCoord(pos);
         const hash = getCellHash(cellCoord);
         const cellKey = hash % (spatialGridSize);
@@ -174,7 +208,7 @@ function getParticleIndexesInRadius(samplePoint) {
         for (let i = startIdx; i < spatialLookupTable.length; i++) {
             if (spatialLookupTable[i].y != cellKey) break;
             const idx = spatialLookupTable[i].x;
-            const dst = positions[idx].distanceTo(samplePoint);
+            const dst = predictedPositions[idx].distanceTo(samplePoint);
             if (dst < options.smoothingRadius) indices.push(idx);
         }
     })
@@ -182,7 +216,7 @@ function getParticleIndexesInRadius(samplePoint) {
 }
 
 function resolveCollisions(i) {
-    let halfBound = new THREE.Vector2(BOUND_X/2, BOUND_Y/2);
+    let halfBound = new THREE.Vector2(options.boundH/2, options.boundV/2);
     halfBound.subVectors(halfBound, new THREE.Vector2(PARTICLE_RADIUS, PARTICLE_RADIUS));
 
     let dirX = Math.sign(positions[i].x);
@@ -221,7 +255,7 @@ function calculateDensity(samplePoint) {
     }
 
     particlesInRadius.forEach(i => {
-        const d = samplePoint.distanceTo(positions[i]);
+        const d = samplePoint.distanceTo(predictedPositions[i]);
         const influence = smoothingKernel(options.smoothingRadius, d);
         density += influence * options.particleMass;
     })
@@ -246,7 +280,7 @@ function calculateSharedPressure(densityA, densityB) {
 
 function calculatePressureForce(particleIdx) {
     let pressureForce = new THREE.Vector3();
-    const samplePoint = positions[particleIdx].clone();
+    const samplePoint = predictedPositions[particleIdx].clone();
     let particlesInRadius = getParticleIndexesInRadius(samplePoint);
 
     if (!options.useSpatialGridLookup) {
@@ -256,10 +290,10 @@ function calculatePressureForce(particleIdx) {
     particlesInRadius.forEach(i => {
         if (i === particleIdx) return;
 
-        const dst = positions[i].distanceTo(samplePoint);
+        const dst = predictedPositions[i].distanceTo(samplePoint);
         let dir = new THREE.Vector3();
         if (dst === 0) dir = getRandomDirection();
-        else dir = positions[i].clone().sub(samplePoint).multiplyScalar(1/dst);
+        else dir = predictedPositions[i].clone().sub(samplePoint).multiplyScalar(1/dst);
         
         const slope = smoothingKernelDerivative(dst, options.smoothingRadius);
         const density = densities[i];
@@ -272,12 +306,18 @@ function calculatePressureForce(particleIdx) {
 
 
 function update(delta) {
-    updateSpatialLookupTable();
 
     // apply gravity and calculate densities
     for (let i=0; i < NUM_PARTICLES; i++) {
         velocities[i].add( new THREE.Vector3( 0, -options.gravity * delta, 0 ) );
-        densities[i] = calculateDensity(positions[i]);
+        predictedPositions[i] = positions[i].clone().add( velocities[i].clone().multiplyScalar( 1 / 60.0 ) );
+    }
+
+    updateSpatialLookupTable();
+
+    //calculate densities
+    for (let i=0; i < NUM_PARTICLES; i++) {
+        densities[i] = calculateDensity(predictedPositions[i]);
     }
 
     // calculate pressure forces
@@ -297,19 +337,30 @@ function update(delta) {
     }
 }
 
+updateSpatialLookupTable();
+
+var labels = {
+    fps: 0
+}
+datgui.add(labels, 'fps').listen();
+
+reset();
+
 var then = 0;
 function step(now) {
-    requestAnimationFrame( step );
-
     now *= 0.001;  // convert to seconds
     const delta = now - then;
     then = now;
+
+    if (paused) return;
+
+    labels.fps = 1 / delta;
     
     if (!isNaN(delta)) update(delta);
     renderer.render( scene, camera );
 }
 
-step();
+renderer.setAnimationLoop(step);
 
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
