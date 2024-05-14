bc19f25e4e37602708b8884f745f6d0f04fa6fb9
panithi.k <panithi.khamwangyang@agoda.com>
2024-05-15 01:39:57 +0700

    feat: presets ready

--- src/js/script.js
+++ src/js/script.js
@@ -17,20 +17,52 @@ const MAX_HEIGHT = 9;
 
 const PARTICLE_DIV = 10;
 const PARTICLE_RADIUS = 0.05;
-const NUM_PARTICLES = 2000;
+const NUM_PARTICLES = 1600;
 const PARTICLE_INIT_WIDTH = 40;
 if (PARTICLE_DIV < 3) throw new Error('PARTICLE_DIV must be greater than 2');
 
 // serializable options
 const options = {
+    gravity: 5,
+    collisionDamping: 0.85,
+    smoothingRadius: 0.35,
+    particleMass: 1,
+    targetDensity: 36,
+    pressureMultiplier: 26,
+    useSpatialGridLookup: true,
+    viscosityStrength: 2,
+    interactionStrength: 330,
+    interactionRadius: 6.3,
+    boundV: MAX_HEIGHT,
+    boundH: MAX_WIDTH
+}
+
+const tankPreset = {
+    gravity: 5,
+    collisionDamping: 0.85,
+    smoothingRadius: 0.35,
+    particleMass: 1,
+    targetDensity: 36,
+    pressureMultiplier: 26,
+    useSpatialGridLookup: true,
+    viscosityStrength: 2,
+    interactionStrength: 330,
+    interactionRadius: 6.3,
+    boundV: MAX_HEIGHT,
+    boundH: MAX_WIDTH
+}
+
+const platePreset = {
     gravity: 0,
     collisionDamping: 0.85,
-    smoothingRadius: 0.45,
+    smoothingRadius: 0.35,
     particleMass: 1,
-    targetDensity: 6.7,
-    pressureMultiplier: 11,
+    targetDensity: 6.6,
+    pressureMultiplier: 18,
     useSpatialGridLookup: true,
-    viscosityStrength: 0.2,
+    viscosityStrength: 0.5,
+    interactionStrength: 250,
+    interactionRadius: 2.8,
     boundV: MAX_HEIGHT,
     boundH: MAX_WIDTH
 }
@@ -42,15 +74,16 @@ var paused = true;
 
 datgui.add(options, 'gravity', 0, 20)
 datgui.add(options, 'collisionDamping', 0, 1)
-datgui.add(options, 'smoothingRadius', 0.1, 2).onChange(() => {
+datgui.add(options, 'smoothingRadius', 0.1, 1).onChange(() => {
     spatialGridRows = Math.ceil(options.boundV / options.smoothingRadius);
     spatialGridCols = Math.ceil(options.boundH / options.smoothingRadius);
 })
 datgui.add(options, 'particleMass', 0.1, 10)
-datgui.add(options, 'targetDensity', 0.1, 30)
-datgui.add(options, 'pressureMultiplier', 0.1, 200)
-datgui.add(options, 'useSpatialGridLookup')
-datgui.add(options, 'viscosityStrength', 0, 2)
+datgui.add(options, 'targetDensity', 0.1, 100)
+datgui.add(options, 'pressureMultiplier', 0.1, 100)
+datgui.add(options, 'viscosityStrength', 0, 10)
+datgui.add(options, 'interactionStrength', 100, 1000)
+datgui.add(options, 'interactionRadius', 0, 10)
 datgui.add(options, 'boundV', 2, MAX_HEIGHT, 1)
 datgui.add(options, 'boundH', 2, MAX_WIDTH, 1)
 
@@ -73,6 +106,19 @@ toggleBtn.onclick = () => {
     pause();
 }
 
+const tankBtn = document.getElementById('tank-preset-btn');
+tankBtn.onclick = () => {
+    Object.assign(options, tankPreset);
+    reset();
+    datgui.updateDisplay();
+}
+
+const plateBtn = document.getElementById('plate-preset-btn');
+plateBtn.onclick = () => {
+    Object.assign(options, platePreset);
+    reset();
+    datgui.updateDisplay();
+}
 
 class Particle {
     constructor(pos) {
@@ -145,7 +191,7 @@ function reset() {
     for (let i = -halfx; i <= halfx; i++) {
         for (let j = -halfy; j <= halfy; j++) {
             if (pidx >= NUM_PARTICLES) break;
-            const particle = new Particle( new THREE.Vector3( i * PARTICLE_RADIUS * 2, j * PARTICLE_RADIUS * 2, 0 ) );
+            const particle = new Particle( new THREE.Vector3( i * PARTICLE_RADIUS * 2.5, j * PARTICLE_RADIUS * 2.5, 0 ) );
             scene.add( particle.sceneObject );
             particles.push( particle );
             positions[pidx] = particle.position.clone();
@@ -157,6 +203,7 @@ function reset() {
     console.log(`Initialized ${particles.length} particles`)
 
     renderer.render( scene, camera );
+    pause(true);
 }
 
 const resetBtn = document.getElementById('reset-btn');
@@ -248,6 +295,21 @@ function resolveCollisions(i) {
     }
 }
 
+function resolvePredictedCollisions(i) {
+    let halfBound = new THREE.Vector2(options.boundH/2, options.boundV/2);
+    halfBound.subVectors(halfBound, new THREE.Vector2(PARTICLE_RADIUS, PARTICLE_RADIUS));
+
+    let dirX = Math.sign(predictedPositions[i].x);
+    let dirY = Math.sign(predictedPositions[i].y);
+
+    if (Math.abs(predictedPositions[i].x) > halfBound.x) {
+        predictedPositions[i].x = halfBound.x * dirX;
+    }
+    if (Math.abs(predictedPositions[i].y) > halfBound.y) {
+        predictedPositions[i].y = halfBound.y * dirY;    
+    }
+}
+
 function smoothingKernel(radius, dist) {
     if (dist >= radius) return 0;
 
@@ -255,9 +317,16 @@ function smoothingKernel(radius, dist) {
     return (radius - dist) * (radius -dist) / vol;
 }
 
+function smoothingKernelSmoothy(radius, dist) {
+    const vol = (Math.PI * Math.pow(radius, 8)) / 4;
+    const value = Math.max(0, radius * radius - dist * dist);
+    return value * value * value / vol;
+}
+
 function viscositySmoothingKernel(dst, radius) {
+    const vol = Math.PI * Math.pow(radius, 8) / 4;
     const value = Math.max(0, radius * radius - dst * dst);
-    return value * value * value;
+    return value * value * value / vol;
 }
 
 function smoothingKernelDerivative(dist, radius) {
@@ -265,6 +334,14 @@ function smoothingKernelDerivative(dist, radius) {
     
     const scale = 12 / (Math.pow(radius, 4) * Math.PI);
     return (dist - radius) * scale;
+}
+
+function smoothingKernelDerivativeSmoothy(dist, radius) {
+    if (dist >= radius) return 0
+
+    const f = radius * radius - dist * dist;
+    const scale = -24 / (Math.pow(radius, 8) * Math.PI);
+    return f * f * scale * dist;
 }
 
 function calculateDensity(samplePoint) {
@@ -327,7 +404,7 @@ function calculatePressureForce(particleIdx) {
 
 function calculateViscosityForce(particleIdx) {
     let viscosityForce = new THREE.Vector3();
-    const samplePoint = predictedPositions[particleIdx].clone();
+    const samplePoint = positions[particleIdx].clone();
 
     let particlesInRadius = getParticleIndexesInRadius(samplePoint);
     if (!options.useSpatialGridLookup) {
@@ -336,12 +413,27 @@ function calculateViscosityForce(particleIdx) {
 
     particlesInRadius.forEach(i => {
         if (i === particleIdx) return;
-        const dst = predictedPositions[i].distanceTo(samplePoint);
+        const dst = positions[i].distanceTo(samplePoint);
         const influence = viscositySmoothingKernel(dst, options.smoothingRadius);
-        viscosityForce.add(velocities[i].clone().sub(velocities[particleIdx]).multiplyScalar(influence * options.particleMass));
+        viscosityForce.add(velocities[i].clone().sub(velocities[particleIdx]).multiplyScalar(influence));
     })
 
-    return viscosityForce * options.viscosityStrength;
+    return viscosityForce.multiplyScalar(options.viscosityStrength);
+}
+
+function interactionForce(inputPos, radius, strength, particleIdx) {
+    const interactF = new THREE.Vector3();
+    const offset = inputPos.clone().sub(positions[particleIdx]);
+    const sqrDst = offset.clone().dot(offset);
+
+    if (sqrDst < radius * radius) {
+        const dist = Math.sqrt(sqrDst);
+        const dirToInput = dist == 0 ? new THREE.Vector3() : offset.clone().multiplyScalar(1/dist);
+        const centerT = 1 - dist / radius;
+        interactF.add(dirToInput.clone().multiplyScalar(strength).sub(velocities[particleIdx].clone()).multiplyScalar(centerT));
+    }
+
+    return interactF;
 }
 
 function interpolateColor(c0, c1, f){
@@ -358,7 +450,7 @@ function update(delta) {
     // apply gravity and calculate densities
     for (let i=0; i < NUM_PARTICLES; i++) {
         velocities[i].add( new THREE.Vector3( 0, -options.gravity * delta, 0 ) );
-        predictedPositions[i] = positions[i].clone().add( velocities[i].clone().multiplyScalar( 1 / 120.0 ) );
+        predictedPositions[i] = positions[i].clone().add( velocities[i].clone().multiplyScalar( 1 / 30.0 ) );
     }
 
     updateSpatialLookupTable();
@@ -370,13 +462,20 @@ function update(delta) {
 
     // calculate pressure forces
     for (let i=0; i < NUM_PARTICLES; i++) {
-        const pressureForce = calculatePressureForce(i);
-        //const viscosityForce = calculateViscosityForce(i);
-        //pressureForce.add(viscosityForce);
-        // a = F / m
-        const acceleration = pressureForce.multiplyScalar(1/densities[i]);
-        velocities[i].add(acceleration.clone().multiplyScalar(delta));
+        if (interactPoint) {
+            const interactF = interactionForce(new THREE.Vector3( interactPoint.x, interactPoint.y, 0 ), options.interactionRadius, options.interactionStrength, i);
+            const interactAcceleration = interactF.multiplyScalar(1/densities[i]);
+            velocities[i].add(interactAcceleration.clone().multiplyScalar(delta));
+        }
 
+        const pressureForce = calculatePressureForce(i);
+        // a = F / m
+        const pressureAcceleration = pressureForce.multiplyScalar(1/densities[i]);
+        velocities[i].add(pressureAcceleration.clone().multiplyScalar(delta));
+
+        const viscosityForce = calculateViscosityForce(i);
+        const viscosityAcceleration = viscosityForce.multiplyScalar(1/densities[i]);
+        velocities[i].add(viscosityAcceleration.clone().multiplyScalar(delta));
     }
 
     // update positions and resolve collisions
@@ -416,7 +515,6 @@ function step(now) {
         raycaster.setFromCamera( mousePosition, camera );
         const intersects = raycaster.intersectObjects([plane]);
         if (intersects.length > 0) {
-            console.log(intersects[0].point);
             interactPoint = intersects[0].point.clone();
         } else {
             interactPoint = null;
